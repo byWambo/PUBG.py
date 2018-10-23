1c26b8b2854c2ffbeef8c53ec7390d4a72112b1c
Matthias <40476420+byWambo@users.noreply.github.com>
2018-10-23 13:57:33 +0200

    Delete client.py

--- pubg/client.py
+++
@@ -1,4 +0,0 @@
-import pubg
-
-pubg.http.connector.HTTP("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI1MDhjNDc3MC1iNzY4LTAxMzYtY2QxOC0wN2NiYmEyMzM3ZDEiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTQwMTMwNzM5LCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6Ii0zNWI5MTdiZS03MzM2LTRiYjEtODgwMC1lOTZiZjkyOTM4YTAifQ.dbCXJY7VfTBIFrqKKBRaVqfE-s6v-Vxdk9Ru79B_UHA",
-                         "https://api.pubg.com/shards/pc-eu/players?filter[playerNames]=L3G3NDx")
